7b811019eebbebd3b38881b26e3cb9b6e9fdb5c0
Ruben Ventura <rventura@betacode.dev>
2024-07-17 12:07:55 -0400

    chore: fix cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import receptorRouter from './routes/receptor.router'
+import cors from 'cors'
 dotenv.config()
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
   return res.send('Hello World!')
